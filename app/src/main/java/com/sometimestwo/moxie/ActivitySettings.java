fcb3e29fd0a31921a67409a2fe5a1da6894e6f78
isaac <iscprz@gmail.com>
2018-09-09 22:05:48 -0500

    Create subcategory settings titles

--- app/src/main/java/com/sometimestwo/moxie/ActivitySettings.java
+++ app/src/main/java/com/sometimestwo/moxie/ActivitySettings.java
@@ -4,20 +4,20 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
+import android.widget.TextView;
 
 import com.sometimestwo.moxie.Utils.Constants;
-
-import java.nio.charset.CharacterCodingException;
 
 public class ActivitySettings extends Activity{
     private final String TAG = this.getClass().getCanonicalName();
 
     private Button mBackButton;
+    private TextView mTextViewAllowNSFW;
+    private TextView mTextViewAllowGifPreview;
     private CheckBox mCheckboxAllowNSFW;
     private CheckBox mCheckboxPreviewGIFHover;
     SharedPreferences prefs_settings;
@@ -42,6 +42,14 @@ public class ActivitySettings extends Activity{
                 prefs_settings_editor.putString(Constants.KEY_ALLOW_NSFW, b ? Constants.SETTINGS_YES : Constants.SETTINGS_NO);
             }
         });
+        // textview clicks will trigger checkbox checking
+        mTextViewAllowNSFW = (TextView) findViewById(R.id.settings_textview_nsfw);
+        mTextViewAllowNSFW.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCheckboxAllowNSFW.setChecked(!mCheckboxAllowNSFW.isChecked());
+            }
+        });
 
         mCheckboxPreviewGIFHover = (CheckBox) findViewById(R.id.settings_checkbox_gif_hover_preview);
         String previewGifHover = prefs_settings.getString(Constants.KEY_GIF_PLAY_HOVER,null);
@@ -51,6 +59,14 @@ public class ActivitySettings extends Activity{
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 prefs_settings_editor.putString(Constants.KEY_GIF_PLAY_HOVER, b ? Constants.SETTINGS_YES : Constants.SETTINGS_NO);
+            }
+        });
+        // textview clicks will trigger checkbox checking
+        mTextViewAllowGifPreview = (TextView) findViewById(R.id.settings_textview_gif_preview);
+        mTextViewAllowGifPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mCheckboxPreviewGIFHover.setChecked(!mCheckboxPreviewGIFHover.isChecked());
             }
         });
 
